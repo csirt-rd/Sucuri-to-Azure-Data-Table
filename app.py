e6c872a8fbd6ec803b7ae007b3dab19360ca1a1b
Julio Gonzalez <92810781+jgonzalez-csirtrd@users.noreply.github.com>
2021-11-29 14:13:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
-import random, string
-import threading
-import requests
-from azure.data.tables import TableClient
+import threading, json, hashlib, base64, hmac, requests
 
-# Azure Data Tables Info
-AZURE_ACC_KEY = ""
-AZURE_ENDPOINT_SUFFIX = "core.windows.net"
-AZURE_ACC_NAME = ""
-AZURE_ENDPOINT = "{}.table.{}".format(AZURE_ACC_NAME, AZURE_ENDPOINT_SUFFIX)
-AZURE_CONN_STR = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix={}".format(
-    AZURE_ACC_NAME, AZURE_ACC_KEY, AZURE_ENDPOINT_SUFFIX
-)
-
+# Azure Log Analytics
+AZURE_WORKSPACE_ID = ""
+AZURE_SHARED_KEY = ""
+AZURE_LOG_TYPE = ""
 # Sucuri Info
-AZURE_TABLE_NAME = ""
 SUCURI_API_URL = "https://waf.sucuri.net/api?v2"
 SUCURI_SITES = []
 
-CHARS = 'abcdef' + string.digits
-
-# Azure Data Tables
-def sucuri_to_azure_table(domain, key, secret, date):
+# Azure Log Analytics
+def sucuri_to_log_analytics(key, secret, date):
     body = requests.post(
         SUCURI_API_URL,
         data={
@@ -30,23 +19,11 @@ def sucuri_to_azure_table(domain, key, secret, date):
             "s": secret,
             "a": "audit_trails",
             "date": date.strftime("%Y-%m-%d"),
-            "format": "json",
-            "limit": 1000
+            "format": "json"
         }
     ).json()
     if len(body) > 2 and len(body) > 6:
         for o in body:
-            ROW_KEY = '-'.join([
-                ''.join(random.choices(CHARS, k=8)),
-                ''.join(random.choices(CHARS, k=4)),
-                ''.join(random.choices(CHARS, k=4)),
-                ''.join(random.choices(CHARS, k=4)),
-                ''.join(random.choices(CHARS, k=12))
-            ])
-            ENTITY_TEMPLATE = {
-                "PartitionKey": domain,
-                "RowKey": ROW_KEY,
-            }
             try:
                 o["request_date"] = date.strftime("%d-%b-%Y")
                 o["request_time"] = datetime.now().strftime("%H:%M:%S")
@@ -55,23 +32,48 @@ def sucuri_to_azure_table(domain, key, secret, date):
             try:
                 del o['geo_location']
             except KeyError:
-                ENTITY = ENTITY_TEMPLATE | o
-                with TableClient.from_connection_string(AZURE_CONN_STR, AZURE_TABLE_NAME) as table_client:
-                    resp = table_client.create_entity(entity=ENTITY)
                 continue
             except TypeError:
                 pass
-            else:
-                ENTITY = ENTITY_TEMPLATE | o
-                with TableClient.from_connection_string(AZURE_CONN_STR, AZURE_TABLE_NAME) as table_client:
-                    resp = table_client.create_entity(entity=ENTITY)
+        body = json.dumps(body)
+        rfc1123date = datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+        x_headers = 'x-ms-date:' + rfc1123date
+        authorization = "SharedKey {}:{}".format(
+            AZURE_WORKSPACE_ID,
+            base64.b64encode(
+                hmac.new(
+                    base64.b64decode(
+                        AZURE_SHARED_KEY
+                    ),
+                    bytes(
+                        '\n'.join([
+                            'POST',
+                            str(len(body)),
+                            'application/json',
+                            x_headers,
+                            '/api/logs']),
+                        encoding='utf-8'),
+                    digestmod=hashlib.sha256
+                ).digest()
+            ).decode()
+        )
+        requests.post(
+            f"https://{AZURE_WORKSPACE_ID}.ods.opinsights.azure.com/api/logs?api-version=2016-04-01",
+            data=body,
+            headers= {
+            'content-type': 'application/json',
+            'Authorization': authorization,
+            'Log-Type': AZURE_LOG_TYPE,
+            'x-ms-date': rfc1123date
+            }
+        )
 
 if __name__ == "__main__":
     yesterday = datetime.now() - timedelta(1)
     threads = list()
     for i in SUCURI_SITES:
         if i["enabled"]:
-            x = threading.Thread(target=sucuri_to_azure_table, args=(i["domain"],i["key"],i["secret"],yesterday), daemon=True)
+            x = threading.Thread(target=sucuri_to_log_analytics, args=(i["key"],i["secret"],yesterday), daemon=True)
             threads.append(x)
             x.start()
     for index, thread in enumerate(threads):
